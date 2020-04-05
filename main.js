1ed914cec154741ee45ca104adad77dd5263361e
mitch3b <mitchkendall@gmail.com>
2020-04-04 20:57:00 -0400

    url

--- main.js
+++ main.js
@@ -13,8 +13,8 @@
   */
 
   //TODO make this configurable...
-  let url = 'http://localhost:5000';
-  //let url = 'https://mitch3a-code-names.herokuapp.com/';
+  //let url = 'http://localhost:5000';
+  let url = 'https://mitch3a-code-names.herokuapp.com/';
   console.log("Using url: " + url);
   const socket = io.connect(url);
 
